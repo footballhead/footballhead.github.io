f16c30f9509e768a05d0ac7e1a24ba220b72c655
Michael Hitchens <mdhitchens@gmail.com>
2014-09-07 18:19:27 -0400

    WIP; was going to do stamps but nevermind!

--- site.js
+++ site.js
@@ -48,6 +48,63 @@ function fadein( element ) {
     requestAnimationFrame( step );
 }
 
+/// Determine the section from a URL.
+///
+/// The section is what appears after the `#` in the URL with the prefix `p`
+/// removed. If we used just `#` then the page would scroll which is
+/// frustrating. If we use `?p=` then the page must refresh which can cause
+/// flashing white in Chrome. If we use `#p[section]` then the page won't
+/// scroll (the section doesn't exist) but the page doesn't need to refresh.
+/// Combine that with an onClick trigger to navigate between sections without
+/// refresh but also allow bookmarking (with an onLoad trigger).
+///
+/// The only problem with this approach is that manipulating the URL through
+/// the browser requires a refresh, but we shouldn't be doing that anyway :P
+///
+/// @param url The URL to parse.
+/// @return The section that the URL is specifying.
+function parseSectionName( url ) {
+    var tokenized = url.split( "#p" );
+    var lastTokIndex = tokenized.length - 1;
+    var section = tokenized[lastTokIndex];
+    if ( existsSectionWithID( section ) ) {
+        return section;
+    } else {
+        return "social";
+    }
+}
+
+/// Display the section specified by the URL.
+function displayRightSection() {
+    var url = document.URL;
+    var sectname = parseSectionName( url );
+    show( sectname );
+}
+
+/// Determine whether or not there exists a section with a certain ID on the
+/// webpage.
+///
+/// @param id The section ID to verify.
+/// @return
+function existsSectionWithID( id ) {
+    var allSectIDs = getAllSectionIDs();
+    return ( allSectIDs.indexOf( id ) !== -1 );
+}
+
+/// Get the IDs of all `<section>` tags in the page.
+///
+/// @return An array of all `<section>` IDs.
+function getAllSectionIDs() {
+    var allsections = document.getElementsByTagName( "section" );
+    var sectIds = [];
+
+    for ( i = 0; i < allsections.length; i++ ) {
+        sectIds[sectIds.length+1] = allsections[i].id;
+    }
+
+    return sectIds;
+}
+
 /// Hide all <section>s and display the one with the given ID.
 ///
 /// Works by manipulating `display` tag. If you add any more <section> tags you
--- todo.md
+++ todo.md
@@ -8,12 +8,31 @@
 * utilities page
 * add email
 * fix fade effect when clicking current page
-* some sort of animation queue and animation controller module?
-* onLoad animations for header and stuff?
-* add "expand" button to games entries?
-* incorporate videos somehow?
-* auto organizing entries photos
-* how to bookmark pages?
 * hide external bar, put emphasis on internal?
 * page descriptions?
-* get rid of test link/lorem before reveal
+* get rid of lorem before reveal
+* direct download links for Ludum Dare entries
+* ?p= sends another HTTP GET request (causes blinking; unecessary)... but # messes with scrolling...
+ - is the ability to bookmark sections necessary?
+ - the current #p breaks consistent font size on Chrome...
+ - add a bookmark link?
+* footer?
+* original release page for gamemaker games
+* icons from Medialoot (http://medialoot.com/item/post-social-media-stamps/)
+* background from seamless paper texture from subtlepatterns.com
+* dropshadow on entry images?
+* icons for github, google+
+
+# WISHLIST
+
+* some sort of animation queue and animation controller module?
+* onLoad animations for header and stuff?
+* add "expand" button to games entries to reveal more info (e.g. release date, programming language, etc)?
+ - would want to put more emphasis on download links
+ - iOS / mac-like "parting of seas" revealing panel?
+* incorporate videos somehow?
+* auto organizing entries photos
+* split sections into different files?
+ - would require AJAX to get...
+* variable width layout?
+* easier way to manage links on transcriptions page
