f3783fd7c21b33c8bf57f412735253dd60352d9e
Michael Hitchens <mdhitchens@gmail.com>
2016-01-19 19:53:44 -0500

    Make games the default tab
    Because that's why people are visiting my portfolio. Make it easy for
    them.

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ window.addEventListener( 'load', function () {
     var tabModel = new TabModel();
     var tabView = new TabView( tabModel );
 
-    tabModel.changeTab( TAB_CONTACT );
+    tabModel.changeTab( TAB_GAMES );
     document.getElementById( 'app-container' ).style.display = 'block';
 });
