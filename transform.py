85200938b29f44ab8e127ea472137a2f58d0310f
Michael Hitchens <mdhitchens@gmail.com>
2016-07-31 23:45:20 -0400

    Add new transcriptions, ability to omit video

--- transform.py
+++ transform.py
@@ -23,7 +23,13 @@ def main( argv ):
         result += "<td>" + tokens[0] + "</td>"
         result += "<td>" + tokens[1] + "</td>"
         result += "<td>" + tokens[2] + "</td>"
-        result += "<td><a href='" + tokens[3] + "'>Video</a></td>"
+        
+        # HACK: Allow the Video field to be empty if there is no video
+        if tokens[3] == '-':
+            result += "<td>-</td>"
+        else:
+            result += "<td><a href='" + tokens[3] + "'>Video</a></td>"
+            
         result += "<td><a href='" + tokens[4] + "'>PDF</a></td>"
         result += "<td><a href='" + tokens[5].strip( "\n" ) + "'>MIDI</a></td>"
         result += "</tr>"
