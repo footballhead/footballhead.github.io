c5037728d33f7dd19c17f2e983cca2d1d7f2899f
Michael Hitchens <mdhitchens@gmail.com>
2017-03-15 20:05:57 -0400

    Sheet music table now in JS, has bookmarkable link
    If the URL ends in #sheet-music it will bring you right to the sheet-music screen.
    
    The sheet music table is now in JavaScript. This means it's easier to manage, also I can add sorting and filtering later.

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ window.addEventListener( 'load', function () {
     var tabModel = new TabModel();
     var tabView = new TabView( tabModel );
 
-    tabModel.changeTab( TAB_GAMES );
-    document.getElementById( 'app-container' ).style.display = 'block';
+    var musicTable = new MusicTableModel();
+    var musicSection = document.getElementById('transcriptions');
+    musicSection.appendChild(musicTable.toDomTable());
+
+    if (window.location.href.endsWith("#sheet-music")) {
+        tabModel.changeTab(TAB_TRANSCRIPTIONS);
+        document.getElementById( 'app-container' ).style.display = 'block';
+    } else {
+        tabModel.changeTab( TAB_GAMES );
+        document.getElementById( 'app-container' ).style.display = 'block';
+    }
 });
