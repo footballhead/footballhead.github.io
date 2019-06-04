9088454fdb46b1c4a70977d8eb9593c36c5a728d
Michael Hitchens <mdhitchens@gmail.com>
2019-06-03 20:25:41 -0400

    Redesign and simplify
    Simplifying to reduce complexity:
    
    * Flatten into one page, no sections
    * Remove (a lot of) unnecessary JS
    * Make text easier to read by using bigger font, more space between lines

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 'use strict';
 
 window.addEventListener( 'load', function () {
-    var tabModel = new TabModel();
-    var tabView = new TabView( tabModel );
-
     var musicTable = new MusicTableModel();
     var musicSection = document.getElementById('transcriptions');
     musicSection.appendChild(musicTable.toDomTable());
-
-    if (window.location.href.endsWith("#sheet-music")) {
-        tabModel.changeTab(TAB_TRANSCRIPTIONS);
-        document.getElementById( 'app-container' ).style.display = 'block';
-    } else {
-        tabModel.changeTab( TAB_GAMES );
-        document.getElementById( 'app-container' ).style.display = 'block';
-    }
 });
